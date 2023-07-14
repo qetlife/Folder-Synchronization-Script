1329f113e91fdfd72ea499a13fd6c695666be324
qetlife <lemejsmurf@gmail.com>
2023-07-14 17:32:13 +0100

    fixing bugs

--- sync.py
+++ sync.py
@@ -3,14 +3,13 @@ import shutil
 import logging
 import schedule
 import threading
-import time
 
-def sync_folders(source_folder, replica_folder):
-    print("SYNC DEBUG")
-    # Remove any items in the replica folder that don't exist in the source folder
-    for item in os.listdir(replica_folder):
-        replica_item = os.path.join(replica_folder, item)
-        source_item = os.path.join(source_folder, item)
+def sync_folders(source, replica):
+
+    # Remove extra files and directories from the replica folder that aren't on the source folder
+    for item in os.listdir(replica):
+        replica_item = os.path.join(replica, item)
+        source_item = os.path.join(source, item)
 
         if not os.path.exists(source_item):
             if os.path.isdir(replica_item):
@@ -20,13 +19,13 @@ def sync_folders(source_folder, replica_folder):
                 os.remove(replica_item)
                 logger.info("Removing the " + item + " item from the replica directory.")
     
-    # Iterate over files and directories in the source folder
-    for item in os.listdir(source_folder):
-        source_item = os.path.join(source_folder, item)
-        replica_item = os.path.join(replica_folder, item)
+    # Iterate over all the files and directories in the source folder
+    for item in os.listdir(source):
+        source_item = os.path.join(source, item)
+        replica_item = os.path.join(replica, item)
 
         if os.path.isdir(source_item):
-            # If the item is a directory, recursively synchronize it
+            # If the item is a directory, call sync_folders again (recursivity)
             if not os.path.exists(replica_item):
                 os.makedirs(replica_item)
                 logger.info("Creating the folder " + item + " to the replica directory.")
@@ -37,50 +36,55 @@ def sync_folders(source_folder, replica_folder):
                 shutil.copy2(source_item, replica_item)
                 logger.info("Copying the " + item + " to the replica file")
 
+
+# Target function for the thread
 def run():
+    logger.info("Started Synchronization.")
     while flag:
         schedule.run_pending()
-        time.sleep(1)
 
-source_folder = "source"
-replica_folder = "replica"
-sync_interval = 1
+# auxiliary variables for the paths.
+source = "source"
+replica = "replica"
+output_log = "output.log"
+sync_interval = 5
 
+#Logging configuration for console
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
 
 logger = logging.getLogger('Sync')
 
-file_handler = logging.FileHandler('output.log')
+#Logging configuration for .log file
+file_handler = logging.FileHandler(output_log)
 file_handler.setFormatter(logging.Formatter('%(asctime)s - %(message)s'))
 file_handler.setLevel(logging.INFO)
 
-# Create a console handler
-#console_handler = logging.StreamHandler()
-#console_handler.setLevel(logging.INFO)
-
-# Create a formatter
-#formatter = logging.Formatter('%(asctime)s - %(message)s')
-
-# Set the formatter for the handlers
-#file_handler.setFormatter(formatter)
-#console_handler.setFormatter(formatter)
-
-# Add the handlers to the logger
 logger.addHandler(file_handler)
-#logger.addHandler(console_handler)
 
+#Setup the periodic call of the sync_folders
+schedule.every(sync_interval).seconds.do(sync_folders, source, replica)
 
-
-schedule.every(5).seconds.do(sync_folders, source_folder, replica_folder)
-
+#flag to end synchronization
 flag = True
-scheduler_thread = threading.Thread(target=run)
-scheduler_thread.start()
 
-while scheduler_thread.is_alive():
-    if input().lower() == 'q':
+#Thread start
+sync_thread = threading.Thread(target=run())
+sync_thread.start()
+
+#Provides the ability for the user to use command line commands for information
+while sync_thread.is_alive():
+    u_input = input()
+    if u_input == 'q':
         flag = False
-        print("Thread stopped")
+        logger.info("Stopped Synchronization.")
         break
+    elif u_input == "path":
+        print("Source folder path: " + source)
+        print("Replica folder path: " + replica)
+    elif u_input == "log":
+        print("Log file path: " + output_log)
+    elif u_input == "interval":
+        print("Synchronization interval: " + str(sync_interval) + " seconds.")
 
-scheduler_thread.join()
+#Stops the thread
+sync_thread.join()
